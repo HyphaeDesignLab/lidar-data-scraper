629f38e7e48f2d82a51de3b7f3b60bfa87fb81ce
Ivan Velev <vanecat@gmail.com>
2023-03-26 14:54:09 -0700

    test script to run in background

--- usgs-scraper/test.py
+++ usgs-scraper/test.py
@@ -1,9 +1,25 @@
+import time
+def check():
+    print('check')
 
-test_var=1
-def testit():
-    test_var=2
-    print(test_var)
+def run_in_bg():
+    last_i = 0
+    for i in range(100):
+        f = open('run_in_bg.txt', 'a')
+        f.write("z" + str(i)+"\n")
+        f.close()
+        time.sleep(.5)
+        last_i = i
+    print('%s lines written' % last_i)
 
-print(test_var)
-testit()
-print(test_var)
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--cmd', dest='cmd', type=str, help='Specify command')
+args = parser.parse_args()
+
+if args.cmd == 'run_in_bg':
+    run_in_bg()
+else:
+    check()
