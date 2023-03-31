fc720910d3c01b0142cde124a95c1966c876bb60
Ivan Velev <vanecat@gmail.com>
2023-03-31 16:36:16 -0700

    make projects list page-able

--- client/src/scraper/Source.js
+++ client/src/scraper/Source.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
+const pageSize = 10;
 const Source = ({model, isCurrent, onShow}) => {
     const onSourceClick = (e) => {
         console.log(model.id, isCurrent);
@@ -9,7 +10,7 @@ const Source = ({model, isCurrent, onShow}) => {
 
     const [projectData, setProjectData] = useState(null);
     const [projects, setProjects] = useState(null);
-    const [projectsFiltered, setProjectsFiltered] = useState(null);
+    const [projectsPaged, setProjectsPaged] = useState(null);
     const [isShow, setShow] = useState(false);
     useEffect(() => {
         if (!isCurrent) {
@@ -31,6 +32,8 @@ const Source = ({model, isCurrent, onShow}) => {
         setShow(true);
     }, [isCurrent]);
 
+    const [page, setPage] = useState(0);
+    const [maxPages, setMaxPages] = useState(-1);
     useEffect(() => {
         if (!projectData) {
             return;
@@ -39,17 +42,30 @@ const Source = ({model, isCurrent, onShow}) => {
         const sortedKeys = Object.keys(projectData.data).sort((a,b)=> { return projectData.data[a].dateModified > projectData.data[b].dateModified ? 1 : -1; })
         const sorted = sortedKeys.map(k => { projectData.data[k].id = k; return projectData.data[k]; });
         setProjects(sorted);
-        setProjectsFiltered(sorted.slice(0, 10));
-        console.log(sorted.slice(0, 10), sorted);
+        setMaxPages(Math.ceil(sorted.length / pageSize));
     }, [projectData]);
 
+    useEffect(() => {
+        if (!projects) {
+            return;
+        }
+        setProjectsPaged(projects.slice(page * pageSize, page * pageSize + pageSize));
+    }, [projects, page]);
+
+    const gotoPage = (direction) => {
+        setPage(p => Math.max(0, Math.min(maxPages, p + direction)));
+    };
     return <div>
         <a href={`/sources/${model.id}`} onClick={onSourceClick}>{model.name}</a>
         <div style={{display: isShow ? '':'none'}}>
-        {isCurrent && !!projectsFiltered &&
-            projectsFiltered.map(project =>
-                <div className={'project'} key={project.id}>{project.id} ({project.dateModified})</div>)
-        }
+            <div><button disabled={page === 0} onClick={gotoPage.bind(null, -1)}>&lt;&lt; Prev</button>
+                &nbsp;
+                <button disabled={page === maxPages - 1} onClick={gotoPage.bind(null, 1)}>Next &gt;&gt;</button>
+            </div>
+            {isCurrent && !!projectsPaged &&
+                projectsPaged.map(project =>
+                    <div className={'project'} key={project.id}>{project.id} ({project.dateModified})</div>)
+            }
         </div>
     </div>;
 };
