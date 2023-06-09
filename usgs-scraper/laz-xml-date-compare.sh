d94b24ba6379c53a7eab0f82254e01e2d73a5e37
Ivan Velev <vanecat@gmail.com>
2023-06-09 11:45:48 -0700

    updated date start/end search str

--- usgs-scraper/laz-xml-date-compare.sh
+++ usgs-scraper/laz-xml-date-compare.sh
@@ -14,15 +14,7 @@ compare_xml_to_laz_date() {
 check_xml_dates_within_project() {
   for fff in projects/*/*/meta/*xml.txt projects/*/meta/*xml.txt; do
     echo $fff | sed -E -e 's@.+/(.+/.+)/meta/[^/]+@\1@' -e 's@.+/(.+)/meta/[^/]+@\1@'
-    sed -nE -e '
-    /begdate:/ {
-      s/begdate://
-      N
-      s/\n/-/
-      s/enddate://
-      p
-    }
-    ' $fff
+    sed -nE -e '/date_start:/ {s/date_start://;N;s/\n/-/;s/date_end://;p;}' $fff
   done
 
 }
