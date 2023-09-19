c28dce1cd6a7e9c760a11194a3a9c100b01dd20d
Ivan Velev <vanecat@gmail.com>
2023-09-19 16:00:18 -0700

    city scrape notes

--- cities/city-scraper.py
+++ cities/city-scraper.py
@@ -1,1 +1,9 @@
 # TODO enter the city scraping algorithm here
+
+# search
+# https://nominatim.openstreetmap.org/search.php?q=los+angeles&format=jsonv2
+# display_name ends on "<full_state_name>, United States"
+# 207359 is the ID to feed into the next API call
+
+# to get full details including polygon
+# https://nominatim.openstreetmap.org/details.php?osmtype=R&osmid=___ID____&class=boundary&&hierarchy=0&group_hierarchy=1&polygon_geojson=1&format=json
