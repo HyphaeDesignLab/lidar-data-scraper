18a66cd97b9849ceacbaa1f52dee4d0d10784867
Ivan Velev <vanecat@gmail.com>
2023-09-22 17:51:53 -0700

    scrape project index: simplify; add file last mod/file size and changes.txt file fodr differences

--- usgs-scraper/scrape-project-index.sh
+++ usgs-scraper/scrape-project-index.sh
@@ -2,25 +2,12 @@ script_base_dir=$(pwd)
 . ./utils-stats.sh
 
 scrape_project_index() {
-
-    if [ ! -d projects/ ]; then
-      mkdir projects;
-    fi
-
     local project_path="projects"
-    project="$1"
-    subproject="$2"
     if [ $project ]; then
         project_path="projects/$project"
-      if [ ! -d $project_path ]; then
-        mkdir $project_path;
-      fi
     fi
-    if [ $subproject ]; then
-        project_path="projects/$project/$subproject"
-      if [ ! -d $project_path ]; then
-        mkdir $project_path;
-      fi
+    if [ ! -d $project_path ]; then
+      mkdir -p $project_path;
     fi
 
     if [ ! -d $project_path/_index ]; then
@@ -34,18 +21,9 @@ scrape_project_index() {
     backup_dir=$project_path/_index/backup/$(date +%Y-%m-%d---%H-%M-%S)
     mkdir $backup_dir
 
-    local project_path_url=""
-    if [ $project ]; then
-      project_path_url="$project/"
-    fi
-    if [ $subproject ]; then
-      project_path_url="$project/$subproject/"
-    fi
-
     ### DOWNLOAD
     base_url=https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects
-    url=$base_url/$project_path_url
-    curl -s -S --retry 4 --retry-connrefused $url 2> $backup_dir/__errors.txt > $backup_dir/_index.html
+    curl -s -S --retry 4 --retry-connrefused $base_url/$project 2> $backup_dir/__errors.txt > $backup_dir/_index.html
     if [ "$(grep '404 Not Found' $backup_dir/_index.html)" ]; then
       echo '404 not found' >> $backup_dir/__errors.txt
     fi
@@ -55,38 +33,50 @@ scrape_project_index() {
         rm $backup_dir/__errors.txt
     fi
 
+    # get directory name and last modified out of HTML
+    #    skip (d=delete) all lines that are not DIR
+    #    get the href PATH and the YYYY-MM-DD HH:MM timestamp and -/12K/1.2M/200M file size
+    #    finally remove slashes in href PATH and trailing - (i.e. missing/not-applicable file size)
     sed -E \
       -e '/<img[^>]+alt="\[DIR\]">/ !d' \
-      -e 's@<img[^>]+alt="\[DIR\]"> *<a href="([^"]+)">[^<]+</a> +([0-9]{4}-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]).+@\1@' \
-      -e 's@/@@' \
-     $backup_dir/_index.html > $backup_dir/tmp.txt
+      -e 's@<img[^>]+alt="\[DIR\]"> *<a href="([^"]+)">[^<]+</a> +([0-9]{4}-[0-9][0-9]-[0-9][0-9]) +([0-9][0-9]:[0-9][0-9]) +([-0-9KMG\.]+).*@\1~\2T\3~\4@' \
+      -e 's@/@@;s/-$//;' \
+     $backup_dir/_index.html > $backup_dir/___dirs_and_details.txt
 
-    grep '_' $backup_dir/tmp.txt > $backup_dir/index.txt
-    if [ ! "$subproject" ]; then
-        python3 $script_base_dir/get-project-year-and-state.py $backup_dir/index.txt > $backup_dir/index_with_year_and_state.txt
-    fi
+
+    # get only directories containing underscore (_); that implies it is a sub-project
+    #   save them to index.txt
+    grep '_' $backup_dir/___dirs_and_details.txt > $backup_dir/index_details.txt
+
+    sed -E -e 's/^([^~]+)~.+/\1/' $backup_dir/index_details.txt > $backup_dir/index.txt
+    sed -E -e 's/^([^~]+)~.+/\1/' $backup_dir/___dirs_and_details.txt > $backup_dir/___dirs.txt
+
+    states_filter="$(tr '\n' '|' < states-to-scrape.txt | sed -E -e 's/\|$//')"
+    python3 $script_base_dir/get-project-year-and-state.py $backup_dir/index.txt | grep -E "~$states_filter~" > $backup_dir/index_with_year_and_state.txt
 
     if [ "$project" ]; then
-      if [ "$(grep -o '^metadata$' $backup_dir/tmp.txt)" ]; then
-        echo metadata > $backup_dir/metadata_dir.txt
+      grep -io '^meta(data)?$' $backup_dir/___dirs.txt 2>/dev/null > $backup_dir/metadata_dir.txt
+      if [ "$?" = "1" ]; then
+        rm $backup_dir/metadata_dir.txt
       fi
-      if [ "$(grep -io '^laz$' $backup_dir/tmp.txt)" ]; then
-        echo laz > $backup_dir/laz_dir.txt
-      fi
-      if [ "$(grep -io '^las$' $backup_dir/tmp.txt)" ]; then
-        echo las > $backup_dir/las_dir.txt
+      grep -io '^la[zs]?$' $backup_dir/___dirs.txt 2>/dev/null > $backup_dir/laz_dir.txt
+      if [ "$?" = "1" ]; then
+        rm $backup_dir/laz_dir.txt
       fi
     fi
 
-    rm $backup_dir/tmp.txt
+    # remove temporary files
+    rm $backup_dir/___*.txt
 
     ### START DIFF/STATS
     ### MAKE STATS / DIFF with previous (currents)
     current_dir=$backup_dir/../../current
     mkdir $backup_dir/diff
     if [ -d $current_dir ]; then
-      diff $current_dir/index.txt $backup_dir/index.txt | grep -E '^<' | sed -E -e 's/^< //' > $backup_dir/diff/removed.txt
-      diff $current_dir/index.txt $backup_dir/index.txt | grep -E '^>' | sed -E -e 's/^> //' > $backup_dir/diff/added.txt
+      # do diffs side-by-side
+      diff --side-by-side $current_dir/index.txt $backup_dir/index.txt | tr -d '\t ' | grep -E '<$' | sed -E -e 's/<$//' > $backup_dir/diff/removed.txt
+      diff --side-by-side $current_dir/index.txt $backup_dir/index.txt | tr -d '\t ' | grep -E '^>' | sed -E -e 's/^>//' > $backup_dir/diff/added.txt
+      diff --side-by-side $current_dir/index_details.txt $backup_dir/index_details.txt | tr -d '\t ' | grep '|' > $backup_dir/diff/changes.txt
 
       sed -E \
        -e 's/~20[0-9]{2}.+$//' \
