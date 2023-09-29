6d2923972742bc6bd117eb8304a2a29e57d72a45
Ivan Velev <vanecat@gmail.com>
2023-09-28 17:51:44 -0700

    migration: comment out code (already migrated)

--- usgs-scraper/migrate.sh
+++ usgs-scraper/migrate.sh
@@ -1,62 +1,31 @@
+. ./utils.sh
 . ./utils-stats.sh
 
 # create and cleanup
 #  create index_details.txt (to contain last-mod and size)
-migrate_backup_dirs() {
-  local project="$1"
-  local project_path='projects'
-  if [ "$project" ]; then
-    project_path="projects/$project"
-  fi
-  local index=($(project_index $project))
-
-  for item_i in ${index[@]}; do
-    echo $project/$item_i:
-    if [ -d $project_path/$item_i/_index/backup ]; then
-      for backup_dir in $(ls -1 $project_path/$item_i/_index/backup/); do
-        echo $backup_dir
-        if [ -d $backup_dir/diff/ ]; then
-          if [ -s $backup_dir/diff/added.txt ]; then
-            mv $backup_dir/diff/added.txt $backup_dir/diff-added.txt;
-          fi
-          if [ -s $backup_dir/diff/removed.txt ]; then
-            mv $backup_dir/diff/removed.txt $backup_dir/diff-removed.txt;
-          fi
-          if [ -s $backup_dir/diff/added.txt ]; then
-            mv $backup_dir/diff/added.txt $backup_dir/diff-added.txt;
-          fi
-          if [ -s $backup_dir/diff/changes.txt ]; then
-            mv $backup_dir/diff/changes.txt $backup_dir/diff-changed.txt;
-          fi
-          rm -rf $backup_dir/diff/
-        fi
-      done
-    fi
-    local current_dir=$project_path/$item_i/_index/current
-    if [ -d current_dir/diff/ ]; then
-      echo $project/$item_i/diff
-      if [ -s $current_dir/diff/added.txt ]; then
-        mv $current_dir/diff/added.txt $current_dir/diff-added.txt;
-      fi
-      if [ -s $current_dir/diff/removed.txt ]; then
-        mv $current_dir/diff/removed.txt $current_dir/diff-removed.txt;
-      fi
-      if [ -s $current_dir/diff/added.txt ]; then
-        mv $current_dir/diff/added.txt $current_dir/diff-added.txt;
-      fi
-      if [ -s $current_dir/diff/changes.txt ]; then
-        mv $current_dir/diff/changes.txt $current_dir/diff-changed.txt;
-      fi
-      rm -rf $current_dir/diff/
-    fi
-    local project_arg=$item_i
-    if [ "$project" ]; then
-       project_arg=$project/$item_i
-     fi
-    migrate_backup_dirs $project_arg
-  done
+cleanup_backup_diffs_dirs() {
+    local ddir="$1"
+    local backup_dir='';
+    for backup_dir in $(ls -1 $ddir); do
+      echo $backup_dir
+#        if [ -d $backup_dir/diff/ ]; then
+#          if [ -s $backup_dir/diff/added.txt ]; then
+#            mv $backup_dir/diff/added.txt $backup_dir/diff-added.txt;
+#          fi
+#          if [ -s $backup_dir/diff/removed.txt ]; then
+#            mv $backup_dir/diff/removed.txt $backup_dir/diff-removed.txt;
+#          fi
+#          if [ -s $backup_dir/diff/added.txt ]; then
+#            mv $backup_dir/diff/added.txt $backup_dir/diff-added.txt;
+#          fi
+#          if [ -s $backup_dir/diff/changes.txt ]; then
+#            mv $backup_dir/diff/changes.txt $backup_dir/diff-changed.txt;
+#          fi
+#          rm -rf $backup_dir/diff/
+#        fi
+    done
 }
 
-if [ "$1" = 'backup_dirs' ]; then
-  migrate_backup_dirs
+if [ "$1" = 'backup_dirs_cleanup' ]; then
+  loop_on_projects '' cleanup_backup_diffs_dirs '%s/_index/backup' 20
 fi
