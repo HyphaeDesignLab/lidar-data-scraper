ef791df5ab18308b2f60c34626f5e0edb5918aaa
Ivan Velev <vanecat@gmail.com>
2023-09-28 18:42:38 -0700

    index scraper: check ONLY for meta/laz; check if matched files are empty (return code of grep won't do)

--- usgs-scraper/scrape-index-helper.sh
+++ usgs-scraper/scrape-index-helper.sh
@@ -88,16 +88,16 @@ scrape_index_helper__parse_index() {
   fi
 
   # get meta/laz/las directories
-  grep -v '_' $download_dir/___dirs_and_details.txt >$download_dir/data_details.txt
+  grep -v '_' $download_dir/___dirs_and_details.txt | grep -Eoi '^(meta(data)?|la[zs])~'> $download_dir/data_details.txt
   if [ $? = 0 ]; then
     grep -Eio '^meta(data)?~' $download_dir/data_details.txt 2>/dev/null | tr -d '~' > $download_dir/metadata_dir.txt
-    if [ "$?" = 0 ]; then
+    if [ -s $download_dir/metadata_dir.txt ]; then
       echo -n data > $download_dir/contents-type.txt
     else
       rm $download_dir/metadata_dir.txt
     fi
-    grep -Eio '^la[zs]?~' $download_dir/data_details.txt 2>/dev/null | tr -d '~' >$download_dir/laz_dir.txt
-    if [ "$?" = 0 ]; then
+    grep -Eio '^la[zs]~' $download_dir/data_details.txt 2>/dev/null | tr -d '~' > $download_dir/laz_dir.txt
+    if [ -s $download_dir/laz_dir.txt ]; then
       echo -n data > $download_dir/contents-type.txt
     else
       rm $download_dir/laz_dir.txt
