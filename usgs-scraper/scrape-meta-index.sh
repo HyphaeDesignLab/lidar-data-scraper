629d4c0cc4c84156dee5f31f955677cba40348b0
Ivan Velev <vanecat@gmail.com>
2023-10-02 16:48:39 -0700

    scrape meta index helper: UPDATED majro refactor

--- usgs-scraper/scrape-meta-index.sh
+++ usgs-scraper/scrape-meta-index.sh
@@ -1,167 +1,170 @@
 . ./utils.sh
 . ./utils-stats.sh
 
-scrape_meta_index() {
-    local project_path="projects/$project"
-    local meta_dir=$project_path/meta
-    local backup_dir=$meta_dir/_index/backup/$(date +%Y-%m-%d---%H-%M-%S)
-    mkdir -p $backup_dir
-    local current_dir=$meta_dir/_index/current/
+scrape_meta_index_helper() {
+  local project="$1"
+  local project_path="projects"
+  if [ $project ]; then
+    project_path="projects/$project"
+  fi
 
-    local project_name=$(cut -d'/' -f 1 <<< $project); # get the project text before '/'
-    local subproject_name=$(cut -d'/' -f 2 <<< $project); # get the subproject (text after a slash /)
-    if [ "$subproject_name" = '' ]; then subproject_name='zzzzzzzzzz____nonexistent_string'; fi; # subproject is used for text replacement below, so if NOT SET, set it to something that cannot possibly exist in a filename
+  #backup_dir=backup/2023-05-29---15-56-46
 
-    echo_if_debug "meta index scrape: $project"
-    # introducing new meta directory structure:
-    # START: post-factum set-up of _index/current and _index/backup
-    #   meta/_index/current/ where all non XML and non .TXT.XML files live
-    #   meta/_index/backup/<backup_datestamp> folder lives of past scrapes (copies of previous _current folders)
-    # if we never did a backup and no current dir, migrate all files from main meta/dir to _current
-    if [ ! -d $current_dir ]; then
-      echo_if_debug 'migrating to new meta directories'
-      mkdir -p $current_dir # make current dir
+  # Make a new back-up dir, download/parse new data into it
+  local backup_dir=$project_path/meta/_index/backup/$(date +%Y-%m-%d---%H-%M-%S)
 
-      # grab last-mod time from meta dir itself
-      local meta_dir_last_mod_in_seconds=$(stat -c '%Y' $meta_dir 2>/dev/null)
-      if [ "$meta_dir_last_mod_in_seconds" = '' ]; then
-        # alternate usage of stat
-        meta_dir_last_mod_in_seconds=$(stat -f "%m" -t "%s" $meta_dir)
-      fi
-      local previous_backup_date=$(date --date="@$meta_dir_last_mod_in_seconds" '+%Y-%m-%d---%H-%M-%S' 2>/dev/null)
-      if [ "$previous_backup_date" = '' ]; then
-        # if date utility does not work with the "--date" arg, try '-r'
-        previous_backup_date=$(date -r "$meta_dir_last_mod_in_seconds" '+%Y-%m-%d---%H-%M-%S' 2>/dev/null)
-      fi
+  # Compare it to the current (about to become last-backup)
+  local current_dir=$project_path/meta/_index/current
 
-      # copy all non .XML and non .TXT.XML files to new current dir
-      for fff in _errors.txt _index.html xml_files.txt xml_files_details.txt zip_files.txt project-length-days.txt; do
-        if [ -f $meta_dir/$fff ]; then
-          mv $meta_dir/$fff $current_dir/$fff 2>/dev/null
-        else
-          echo > $current_dir/$fff
+  # DOWNLOAD
+  scrape_meta_index_helper__curl $project $backup_dir
+
+  # Parse data out of index.html
+  scrape_meta_index_helper__parse_index $project $backup_dir
+
+  # Do diff with previous backup (aka current current)
+  scrape_meta_index_helper__diff $backup_dir $current_dir
+
+  # Backup: make new back-up => current
+  scrape_meta_index_helper__backup $backup_dir $current_dir
+}
+
+scrape_meta_index_helper__curl() {
+  local project="$1"
+  local download_dir="$2"
+  mkdir -p $download_dir
+
+  local meta_url_dir=$(cat $project_path/_index/current/metadata_dir.txt)
+
+  local base_url=https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects/
+  if [ "$LIDAR_SCRAPER_DEBUG" != '' ]; then
+    base_url=$LIDAR_SCRAPER_DEBUG__MOCK_SERVER_ADDRESS
+    echo_if_debug "scrape-index-helper.sh: mock server in use: $LIDAR_SCRAPER_DEBUG__MOCK_SERVER_ADDRESS"
+  fi
+
+  curl_scrape $base_url/$project/$meta_url_dir/index.html $download_dir/index.html $download_dir/___http_code.txt $download_dir/___errors.txt
+  if [ $? != 0 ]; then
+    # save more detailed error to errors.txt (only 1 request (index.html) error we are dealing with here)
+    date > $download_dir/errors.txt
+    cat $download_dir/___errors.txt >> $download_dir/errors.txt
+    echo -n 'HTTP response: ' >> $download_dir/errors.txt
+    cat $download_dir/___http_code.txt >> $download_dir/errors.txt
+  else
+    # if no errors
+    if [ "$LIDAR_SCRAPER_DEBUG" = '' ]; then
+      # if not debugging, save index.html to server_mock dir for local testing
+      mkdir -p projects/_server_mock/$project/$meta_url_dir/
+      cp $download_dir/index.html projects/_server_mock/$project/$meta_url_dir/index.html
+    fi
+  fi
+  # remove temporary files
+  rm $download_dir/___*
+}
+
+scrape_meta_index_helper__parse_index() {
+  local project="$1"
+  local download_dir="$2"
+
+  # get meta file name, last modified, and size out of HTML
+  # get ZIP file name if it exists, last modified, and size out of HTML
+  #    skip (d=delete) all lines that are not DIR
+  #    get the href PATH and the YYYY-MM-DD HH:MM timestamp and -/12K/1.2M/200M file size
+  #    finally remove slashes in href PATH and trailing - (i.e. missing/not-applicable file size)
+  grep -oE '<img[^>]+compressed.gif[^>]+> *<a href="([^"]+)">' $download_dir/index.html > $download_dir/___zip_files_details.txt
+  if [ $? = 0 ]; then
+    sed -E -e 's@.*<a href="([^"]+)">[^<]+</a> +([0-9]{4}-[0-9][0-9]-[0-9][0-9]) +([0-9][0-9]:[0-9][0-9]) +([-0-9KMG\.]+).*@\1~\2T\3~\4@' \
+     $download_dir/___zip_files_details.txt > $download_dir/zip_files_details.txt;
+
+     grep -Eo '^[^~]+' $download_dir/zip_files_details.txt > $download_dir/zip_files.txt;
+  fi
+
+
+  local project_name=$(cut -d'/' -f 1 <<< $project); # get the project text before '/'
+  local subproject_name=$(cut -d'/' -f 2 <<< $project); # get the subproject (text after a slash /)
+  if [ "$subproject_name" = '' ]; then subproject_name='zzzzzzzzzz____nonexistent_string'; fi; # subproject is used for text replacement below, so if NOT SET, set it to something that cannot possibly exist in a filename
+
+  grep -E '<img[^>]+alt="\[TXT\]"> *<a href="([^"]+).xml">' $download_dir/index.html > $download_dir/___xml_files_details.txt;
+  if [ $? = 0 ]; then
+    sed -E \
+    -e 's@<img[^>]+alt="\[TXT\]"> *<a href="([^"]+).xml">[^<]+</a> +([0-9]{4}-[0-9][0-9]-[0-9][0-9]) +([0-9][0-9]:[0-9][0-9]) +([-0-9KMG\.]+).*@\1~\2T\3~\4@' \
+    -e 's@/@@' \
+    -e "s/USGS_LPC_/{u}/" \
+    -e "s@$project_name@{prj}@" \
+    -e "s@$subproject_name@{sprj}@" \
+    $download_dir/___xml_files_details.txt > $download_dir/xml_files_details.txt;
+
+    grep -Eo '^[^~]+' $download_dir/xml_files_details.txt > $download_dir/xml_files.txt;
+  fi
+
+  # remove temporary files
+  rm $download_dir/___*
+}
+
+scrape_meta_index_helper__diff() {
+  local backup_dir="$1"
+  local current_dir="$backup_dir/../../current"
+
+  ### START DIFF/STATS
+  local data_type=$(cat $backup_dir/contents-type.txt)
+  ### MAKE STATS / DIFF with previous (currents)
+  if [ -d $current_dir ]; then
+    # diff on xml (meta) index
+    if [ -f $backup_dir/xml_files_details.txt ]; then
+      python3 diff_file_list.py $current_dir/xml_files_details.txt $backup_dir/xml_files_details.txt $backup_dir/
+    fi
+
+   # diff on zip files
+    if [ -f $backup_dir/zip_files_details.txt ]; then
+      python3 diff_file_list.py $current_dir/zip_files_details.txt $backup_dir/zip_files_details.txt $backup_dir/
+    fi
+  else
+    echo 'first time scraping' > $backup_dir/diff.txt
+  fi
+  ### END DIFF/STATS
+}
+
+scrape_meta_index_helper__backup() {
+  local backup_dir="$1"
+  local current_dir="$backup_dir/../../current"
+
+  if [ -d $current_dir ]; then
+    rm -rf $current_dir
+  fi
+  cp -r $backup_dir $current_dir
+}
+
+if [ "$(basename $0)" = "scrape-meta-index-helper.sh" ]; then
+    if [ "$1" = 'main' ]; then
+      scrape_meta_index_helper $1
+    elif [ "$1" = 'loop' ]; then
+      if [ ! "$2" ] || [ ! "$3" ] || [ ! "$4" ]; then
+        echo ' call the "loop" tester with 5 arguments'
+        echo '   1="loop" to invoke the tester'
+        echo '   2=callback_function_to_test and call for every loop '
+        echo '       possible value: '
+        echo '          get           => get from URL  (scrape_meta_index_helper__curl)'
+        echo '          parse         => parse index  (scrape_meta_index_helper__parse_index)'
+        echo '          diff          => diff the previous and current index (scrape_meta_index_helper__diff)'
+        echo '          backup        => backup the current (scrape_meta_index_helper__backup)'
+        echo '   3=project  to loop on: possible values: "all", "project" or "project/subproject"'
+        echo '   4=arg_format  to pass to callback fn: e.g. "%s/_index/backup/2023-04-05"'
+        echo '   5=limit  of loop iterations'
+        echo '   6=is recursive loop (yes/no)'
+      else
+        ___lidar_scrape_project="$3"
+        if [ ! "$___lidar_scrape_project" ]; then
+          ___lidar_scrape_project='all';
         fi
-      done
-      # do a back-up of current to previous (non-existent until now) backup
-      cp -rf $current_dir $meta_dir/_index/backup/$previous_backup_date
+        # $2 will contain the callback fn label
+        if [ "$2" = 'get' ]; then
+          loop_on_projects $___lidar_scrape_project scrape_meta_index_helper__curl $4 $5 $6
+        elif [ "$2" = 'parse' ]; then
+          loop_on_projects $___lidar_scrape_project scrape_meta_index_helper__parse_index $4 $5 $6
+        elif [ "$2" = 'diff' ]; then
+          loop_on_projects $___lidar_scrape_project scrape_meta_index_helper__diff $4 $5 $6
+        elif [ "$2" = 'backup' ]; then
+          loop_on_projects $___lidar_scrape_project scrape_meta_index_helper__backup $4 $5 $6
+        fi
+      fi
     fi
-    #/ END: post-factum set-up of _index/current and _index/backup
-
-    local meta_url_dir=$(cat $project_path/_index/current/metadata_dir.txt)
-
-    ### DOWNLOAD
-    local base_url=https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects
-    if [ "$LIDAR_SCRAPER_DEBUG" != '' ]; then
-      base_url=$LIDAR_SCRAPER_DEBUG__MOCK_SERVER_ADDRESS
-    fi
-    # make sure there is a trailing, else USGS server 301-http-redirects
-    local url=$base_url/$project/$meta_url_dir/
-    curl -s -S --retry 4 --retry-connrefused $url 2> $backup_dir/__errors.txt > $backup_dir/_index.html
-    if [ "$(grep '404 Not Found' $backup_dir/_index.html)" ]; then
-      echo '404 not found' >> $backup_dir/__errors.txt
-    fi
-    if [ $(get_line_count_or_empty $backup_dir/__errors.txt) ]; then
-        date | xargs echo -n >> $backup_dir/_errors.txt
-        cat $backup_dir/__errors.txt >> $backup_dir/_errors.txt
-    fi
-    rm $backup_dir/__errors.txt
-
-    grep -E '<img[^>]+compressed.gif[^>]+> *<a href="([^"]+)">' $backup_dir/_index.html |
-     sed -E -e 's@<img[^>]+compressed.gif[^>]+> *<a href="([^"]+)">.+@\1@' \
-     > $backup_dir/zip_files.txt
-
-    grep -E '<img[^>]+alt="\[TXT\]"> *<a href="([^"]+).xml">' $backup_dir/_index.html |
-     sed -E \
-      -e 's@<img[^>]+alt="\[TXT\]"> *<a href="([^"]+).xml">[^<]+</a> +([0-9]{4}-[0-9][0-9]-[0-9][0-9]) +([0-9][0-9]:[0-9][0-9]) +([-0-9KMG\.]+).*@\1~\2T\3~\4@' \
-      -e 's@/@@' \
-      -e "s/USGS_LPC_/{u}/" \
-      -e "s@$project_name@{prj}@" \
-      -e "s@$subproject_name@{sprj}@" \
-      > $backup_dir/xml_files_details.txt
-
-    grep -Eo '^[^~]+' $backup_dir/xml_files_details.txt > $backup_dir/xml_files.txt
-
-    # get XML file remove, added and date/size difference
-    # compare only file names (in xml_files.txt)
-    #   whether xml file was there but IS NOT NOW OR was not there but IS NOW
-    diff --side-by-side $current_dir/xml_files.txt $backup_dir/xml_files.txt | tr -d '\t ' | grep -E '<$' | sed -E -e 's/<$//' > $backup_dir/removed.txt
-    diff --side-by-side $current_dir/xml_files.txt $backup_dir/xml_files.txt | tr -d '\t ' | grep -E '^>' | sed -E -e 's/^>//' > $backup_dir/added.txt
-    # last-mod date and file size differences live in xml_files_details.txt
-    diff --side-by-side $current_dir/xml_files_details.txt $backup_dir/xml_files_details.txt | tr -d '\t ' | grep '|' > $backup_dir/changes.txt
-
-    # copy current backup as the "current"
-    cp -rf $backup_dir $current_dir
-}
-
-xml_check_empty_or_not_found() {
-  if [ -s $1 ]; then
-    grep -HF '404 Not Found' $1 | sed 's/404 Not Found/not_found/';
-  else
-    echo $1:empty
-  fi;
-}
-xml_file_list() {
-  find projects/ -type f -path '*/meta/*' -name '*.xml'
-}
-scrape_meta_check_empty() {
-  if [ ! "$1" ]; then
-    echo "to check all files and then do counts: $0 check_empty filein fileout"
-    echo " OR"
-    echo "to ONLY show counts: $0  check_empty file"
-    return 1
-  fi
-
-  filein=$1
-  fileout=$2
-  if [ "$2" ]; then
-    if [ ! -f $filein ]; then
-      xml_file_list > $filein
-    fi;
-    while read -r line; do xml_check_empty_or_not_found $line; done < $filein > $fileout
-  else
-    fileout=$1
-  fi
-
-  if [ ! -f $fileout ]; then
-    echo "no such file $fileout";
-    return;
-  fi;
-
-  echo -n 'not found: ';
-  grep -c 'not_found$' $fileout;
-
-  echo -n 'empty: ';
-  grep -c 'empty$' $fileout;
-}
-check_missing_projects() {
-  if [ ! "$1" ] || [ ! "$2" ]; then
-    echo "$0 check_missing_projects <project_list_file> <status_file_name>";
-    echo " where <status_file_name> and <status_file_name>.error will be saved to each <project>/meta/ dir when requesting usgs.gov/<prj>/meta/index.html"
-    return 1;
-  fi
-  for prj in $(cat $1); do
-    curl -s -S --retry 4 --retry-connrefused https://rockyweb.usgs.gov/vdelivery/Datasets/Staged/Elevation/LPC/Projects/$prj/metadata/ 2>projects/$prj/meta/$2.error 1>projects/$prj/meta/$2
-    sleep .5
-  done
-
-  for prj in $(cat $1); do
-    grep -HF '404 Not Found' projects/$1/meta/$2
-  done
-}
-
-if [ "$(basename $0)" = "scrape-meta-index.sh" ]; then
-  if [ ! "$1" ]; then
-    echo "to scrape all meta files: $0 all|<project> <?subproject>"
-    echo " OR"
-    echo "to check if empty meta files: $0  check_empty ..."
-    echo "to check projects that have been taken offline: $0  check_missing_projects ..."
-    exit;
-  fi
-  if [ "$1" = "check_empty" ]; then
-    scrape_meta_check_empty $2 $3
-  elif [ "$1" = "check_missing_projects" ]; then
-    check_missing_projects $2 $3
-  else
-    scrape_meta_index $1
-  fi
 fi
