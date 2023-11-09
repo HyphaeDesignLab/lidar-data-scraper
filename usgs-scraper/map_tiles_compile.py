cecf1fb87939c933ab7c33cbd5f4a93074d7b6c3
Ivan Velev <vanecat@gmail.com>
2023-11-09 10:37:11 -0800

    map tile compile; ignore missing laz sizes

--- usgs-scraper/map_tiles_compile.py
+++ usgs-scraper/map_tiles_compile.py
@@ -65,9 +65,10 @@ def get_geojson_feature_collection_for_project(project, leaves_on_off, all_tiles
     for file_name in xml_files_list_file:
         file_count=file_count+1
 
+    laz_details = {'size':'', 'date_modified':''}
     laz_files_list_filename = laz_dir+'/_index/current/files_details.txt'
+    if os.path.isfile(laz_files_list_filename):
     laz_files_list_file = open(laz_files_list_filename)
-    laz_details = {}
     for line_details in laz_files_list_file:
         line_details = line_details.replace('\n', '')
         laz_details_i = line_details.split('~')
