e07b330197dc6f860e2091549c0c6da961f996a9
Ivan Velev <vanecat@gmail.com>
2023-12-12 18:05:54 -0800

    fixed server tile editing requests

--- usgs-scraper/server.py
+++ usgs-scraper/server.py
@@ -7,6 +7,7 @@ import json
 import ssl
 from datetime import datetime
 import requests
+from urllib.parse import parse_qs
 
 def get_env():
     env = {}
@@ -31,8 +32,11 @@ def get_env():
 env = get_env()
 
 class ScraperServer(http.server.SimpleHTTPRequestHandler):
+
     def do_POST(self):
+        self.read_query('post')
         if not self.check_secret():
+            print('no secret')
             return
         if self.path.startswith('/map-tile-edit'):
             self.edit_map_tile()
@@ -40,11 +44,26 @@ class ScraperServer(http.server.SimpleHTTPRequestHandler):
             self.blank_response()
 
     def do_GET(self):
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
+        self.read_query('get')
+        if self.path.startswith('/test/map-tile-edit'):
+            self.test_edit_map_tile_form()
+        else:
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def read_query(self, method='get'):
+        if method == 'post':
+            content_length = int(self.headers['Content-Length'])
+            print(content_length)
+            self.query = self.rfile.read(content_length).decode('utf-8')
+        else:
+            path_and_query = self.path.split('?') if self.path else [None, None]
+            if len(path_and_query) < 2 or not path_and_query[1]:
+                self.query = None
+            else:
+                self.query = path_and_query[1]
 
     def check_secret(self):
-        path_and_query = self.path.split('?') if self.path else [None, None]
-        if len(path_and_query) < 2 or not path_and_query[1] or  not 'secret='+env['secret'] in path_and_query[1]:
+        if not self.query or not 'secret='+env['secret'] in self.query:
             self.bad_secret_code()
             return False
         return True
@@ -62,24 +81,44 @@ class ScraperServer(http.server.SimpleHTTPRequestHandler):
         self.wfile.write('n/s'.encode('utf-8'))
 
     def edit_map_tile(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
+        parsed_query = parse_qs(self.query)
+        json_string = parsed_query.get('json', [])[0]
+        project = parsed_query.get('project', [])[0]
 
-        print(post_data)
+
+        file_name = f'projects/{project}/map_tiles.json'
+
+        # backup, if it exists
+        if os.path.isfile(file_name):
+            f = open(file_name)
+            now_string = datetime.now().strftime("%Y%m%d_%H%M%S")
+            f_copy = open(f'projects/{project}/map_tiles_{now_string}.json', 'w')
+            f_copy.write(f.read())
+            f_copy.close()
+            f.close()
+
+        # write new
+        f = open(file_name, 'w')
+        f.write(json_string)
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write('ok'.encode('utf-8'))
 
-def start_server():
-    with socketserver.TCPServer(("", int(env['port'])), ScraperServer) as httpd:
+    def test_edit_map_tile_form(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        html = f'<form method="post" action="/map-tile-edit"><input name=secret value="{env["secret"]}"/><input name=project value="_test"/><input name=json value="[1,2,3]"/><input type="submit" /></form>'
+        self.wfile.write(html.encode('utf-8'))
+
+def start_server(custom_port=None):
+    port = int(custom_port) if custom_port else int(env['port'])
+    with socketserver.TCPServer(("", port), ScraperServer) as httpd:
         if 'ssl' in env and env['ssl'] == 'true':
             httpd.socket = ssl.wrap_socket(httpd.socket, keyfile=env['ssl_cert_key_file'], certfile=env['ssl_cert_file'], server_side=True)
-        print("Server running at http://localhost:{}".format(int(env['port'])))
+        print("Server running at http://localhost:{}".format(port))
         httpd.serve_forever()
 
 def start_job(type, project, indeces):
@@ -87,7 +126,7 @@ def start_job(type, project, indeces):
 
     # prefix:              type__project__subproject__
     # full name with date: type__project__subproject__date.txt
-    job_dirname_prefix = f'{type}__{project.replace('/', '__')}'
+    job_dirname_prefix = f"{type}__{project.replace('/', '__')}"
 
     import glob
     if len(glob.glob(f'jobs/active/{job_dirname_prefix}__*/')) > 0:
@@ -168,6 +207,6 @@ def is_valid_number(s):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'run':
-            start_server()
+            start_server(sys.argv[2] if len(sys.argv) > 2 else None)
         elif sys.argv[1] == 'test_start_job':
             start_job(sys.argv[2], sys.argv[3], sys.argv[4])
