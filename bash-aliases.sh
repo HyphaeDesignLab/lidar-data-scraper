3b979b0c53c3f02a8daa83952213aff1e682bb98
Ivan Velev <vanecat@gmail.com>
2024-02-20 17:50:48 -0800

    hyphae lidar server: uses PM2 node/npm module now

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -67,38 +67,21 @@ hyphae_server_pid() {
   ps aux | grep server.py | grep -v grep | sed -E -e 's/^([^ ]+) +([0-9]+) .+/\2/'
 }
 hyphae_server_status() {
-  local pid=$(hyphae_server_pid)
-  if [ "$pid" ]; then
-    echo "server is running: pid $pid"
-  else
-    echo 'server is NOT running'
-  fi
+  pm2 status lidar-scraper-server
+  pm2 describe lidar-scraper-server
 }
 hyphae_server_stop() {
-  local pid=$(hyphae_server_pid)
-  if [ "$pid" ]; then
-    echo "stopping $pid"
-    kill -9 $pid
-  else
-    echo 'no server process to stop'
-  fi
+  pm2 stop lidar-scraper-server
 }
 hyphae_server_start() {
-  local pid=$(hyphae_server_pid)
-  if [ ! "$pid" ]; then
-    nohup python3 $_HOME_DIR_LIDAR_SCRAPER/usgs-scraper/server.py run 1>>$_HOME_DIR_LIDAR_SCRAPER/usgs-scraper/map_server.log 2>>$_HOME_DIR_LIDAR_SCRAPER/usgs-scraper/map_server.error &
-  else
-    echo "server is already running: pid $pid"
-  fi
+  pm2 start lidar-scraper-server
 }
 
 hyphae_server_check_and_restart() {
   local url=$(grep 'server_url' $_HOME_DIR_LIDAR_SCRAPER/usgs-scraper/.env | sed -e 's/server_url=//' | tr -d '\n')
 
   if ! curl --retry 1 --max-time 5  --connect-timeout 3 -f -s $url/map.html >/dev/null 2>/dev/null; then
-    hyphae_server_stop >/dev/null;
-    sleep 20; # sleep because the port in use might need to be released before restart
-    hyphae_server_start >/dev/null;
+    pm2 restart lidar-scraper-server
   fi;
 }
 
