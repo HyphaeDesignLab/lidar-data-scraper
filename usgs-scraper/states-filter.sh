e8f39c739c3afd3d448dae0c29bf1e27a4606c7f
Ivan Velev <vanecat@gmail.com>
2024-03-01 15:24:34 -0800

    states filter

---
+++ usgs-scraper/states-filter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+filter_project_list() {
+  local state_abbr=$1
+  local state_name_and_abbr;
+  local state_name;
+  local grep_pattern;
+  
+  state_name_and_abbr=$(grep "~$1" states.txt)
+  state_name=$(cut -d'~' -f1 <<< $state_name_and_abbr)
+  grep_pattern="(_${state_abbr}_|\b${state_abbr}_|_${state_abbr}\b|\b${state_abbr}\b|$state_name)"
+
+  find projects -maxdepth 4 -mindepth 3 -type d -name 'meta' | sed -E -e 's@projects/@@;s@/meta@@;' | grep -iE "$grep_pattern"
+}
+
+filter_project_list $1
