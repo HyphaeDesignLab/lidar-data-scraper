2f46b7c24ec1cda4e3f8a576d020bb93b79df992
Ivan Velev <vanecat@gmail.com>
2024-03-01 18:09:11 -0800

    added file input; better project/meta removal pattern

--- usgs-scraper/states-filter.sh
+++ usgs-scraper/states-filter.sh
@@ -2,9 +2,10 @@
 
 filter_project_list() {
   local state_abbr=$1
+  local input_file=$2
 
   if [ ! "$state_abbr" ]; then
-    find projects/ -maxdepth 4 -mindepth 3 -type d -name 'meta' | sed -E -e 's@projects/+@@;s@/meta@@;'
+    find projects/ -maxdepth 4 -mindepth 3 -type d -name 'meta' | sed -E -e 's@(^|.+/)projects/+@@;s@/meta$@@;'
     return 0
   fi
 
@@ -16,8 +17,11 @@ filter_project_list() {
   state_name=$(cut -d'~' -f1 <<< $state_name_and_abbr)
   grep_pattern="(_${state_abbr}_|\b${state_abbr}_|_${state_abbr}\b|\b${state_abbr}\b|$state_name)"
 
-
-  find projects/ -maxdepth 4 -mindepth 3 -type d -name 'meta' | sed -E -e 's@projects/+@@;s@/meta@@;' | grep -iE "$grep_pattern"
+  if [ "$input_file" ] && [ -f "$input_file" ]; then
+    cat $input_file | sed -E -e 's@(^|.+/)projects/+@@;s@/meta$@@;' | grep -iE "$grep_pattern"
+  else
+    find projects/ -maxdepth 4 -mindepth 3 -type d -name 'meta' | sed -E -e 's@(^|.+/)projects/+@@;s@/meta$@@;' | grep -iE "$grep_pattern"
+  fi
 }
 
 filter_project_list $1
